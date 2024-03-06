ceb9637b84007e8021beb85bacb4e73c1a884914
Bailzzy <112415662+Bailzzy@users.noreply.github.com>
2024-03-06 17:58:43 -0500

    Updated code

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 
 app = Flask(__name__)
-mongo_client = MongoClient("mongo") 
+mongo_client = MongoClient("localhost") 
 db = mongo_client["Theranos"] 
 users = db["users"]
 
@@ -13,14 +13,23 @@ users = db["users"]
 @app.route('/',methods=["GET"])
 def index():
     print("We here")
-    return render_template('/index.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    currmeth = request.method
+    if currmeth == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = users.find_one({"username": username})
+
+        if user and bcrypt.checkpw(password, user["password"]):
+            flash("You are now logged in")
     return render_template('login.html')
 
 @app.route('/signup', methods=["GET", "POST"])
-def singup():
+def signup():
     currmeth = request.method
     if currmeth == "POST":
         username = request.form["username"]
@@ -33,7 +42,10 @@ def singup():
         return render_template('login.html')
     return render_template('signup.html')
     
-
+@app.route('/style',methods=["GET", "POST"])
+def style():
+    print("We here")
+    return render_template('style.css')
 
 
 #On deployment change localhost to 0.0.0.0
